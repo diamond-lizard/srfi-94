de278db2b91d5d7beeb2bd358969c1820ad60896
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-21 17:37:54 -0800

    imports, register-feature! and include of srfi-94-impl.scm

--- srfi-94.scm
+++ srfi-94.scm
@@ -1,5 +1,15 @@
-(import 'srfi-60)          ;ash, integer-length
+(module srfi-94 ()
+  (import scheme)
+  (import (chicken base))
+  (import (chicken module))
+  (import (chicken platform))
 
-; srfi-23 is already part of Chicken core, so no need to import it
-;(import 'srfi-23)          ;error
+  (import 'srfi-60)          ;ash, integer-length
+
+  ; srfi-23 is already part of Chicken core, so no need to import it
+  ;(import 'srfi-23)          ;error
+
+  (register-feature! 'srfi-94)
+
+  (include "srfi-94-impl.scm"))
 
